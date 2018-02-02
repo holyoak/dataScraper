7d08a3b1ab63c0c46796631ad375880e8e257da8
holyoak <mericsmith@yahoo.com>
2018-02-02 00:17:58 -0800

    add mongo connection add a custom time interval instance

--- src/server/index.js
+++ src/server/index.js
@@ -9,41 +9,65 @@ const MongoClient = require('mongodb').MongoClient
 //
 //  this section needs to come from a config file
 //
+const starter = '2018-02-01T22:00:00-00:00'
+const ender   = '2018-02-02T00:00:00-00:00'
+
 //scraper target
+const targetExchange = 'GDAX'
+const targetProduct = 'BCH-USD'
 const targetAPIurl = 'api.gdax.com'
-const targetProduct = '/products/ETH-USD/candles?granularity=60'
+const targetProductString = '/products/BCH-USD/candles?granularity=60&start=' + starter + '&end=' + ender
 // dB info
 const dbUrl = 'mongodb://localhost:27017'
 const dbName = 'scraper'
+
+const step = 7200
 //  end config section
+//  
+MongoClient.connect(dbUrl, function (err, client) {
+  console.log('connecting to mongo')
+  if (err) console.log(chalk.red(err))
+  else {
+    const db = client.db(dbName)
+    const collection = db.collection(targetExchange + '-' + targetProduct)
+    init(collection, client)
+    console.log(chalk.green('Connected successfully to ' + dbName + ' database'))
+  }
+})
 
-init()
-function init() {
-
-
-const options = {
-  hostname: targetAPIurl,
-  path: targetProduct,
-  method: 'GET',
-  agent: false,
-  headers: {
-    'User-Agent': 'something',
-  },
-}
-
-const req = https.request(options, res => {
-  console.log('statusCode:', res.statusCode)
-  console.log('headers:', res.headers)
-
-  res.on('data', d => {
-    process.stdout.write(d)
+function init(db, client) {
+  const options = {
+    hostname: targetAPIurl,
+    path: targetProductString,
+    method: 'GET',
+    agent: false,
+    headers: {
+      'User-Agent': 'something',
+    },
+  }
+  let data = ''
+  
+  const req = https.request(options, res => {
+    console.log('statusCode:', res.statusCode)
+    
+    res.on('data', d => {data += d})
+    res.on('end', () => {
+      data = JSON.parse(data)
+      data.forEach((d, i)=>{
+        console.log('insert #' + i)
+        db.insertOne({
+          time: d[0],
+          low: d[1],
+          high: d[2],
+          open: d[3],
+          close: d[4],
+          volume:d[5]
+        })
+      })
+    })
   })
-})
-
-req.on('error', e => {
-  console.error(chalk.red(e))
-})
-req.end()
+  req.on('error', e => {console.error(chalk.red(e))})
+  req.end()
   // const publicClient = new Gdax.PublicClient()
   // publicClient.getProductTradeStream('BTC-USD', 8408000, 8409000)
   //   .then((data)=>{
