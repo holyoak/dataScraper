f35ff21d9e1af93ced12181554c2fb3c7846af01
holyoak <mericsmith@yahoo.com>
2018-04-05 01:22:55 -0700

    slight refactor

---
+++ src/index.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+'use strict'
+const chalk = require('chalk')
+const https = require('https')
+const fs = require('fs')
+const MongoClient = require('mongodb').MongoClient
+
+const config = require('../config.json')
+const utils = require('./utils')
+
+let timeStamp = new Date()
+const app = {}
+
+MongoClient.connect(config.dbUrl, function (err, client) {
+  console.log('connecting to mongo')
+  if (err) {
+    console.log('MonggDB error')
+    console.log(chalk.red(err))
+    utils.log('MonggDB error: ' + err)
+    utils.endProcess(1, null)
+  }
+  else {
+    const db = client.db(config.dbName)
+    const collection = db.collection(config.targetExchange + '-' + config.targetProduct)
+    app.client = client
+    app.db = collection
+    const message = 'Starting data-minion history for '
+      + config.targetExchange + '/' + config.targetProduct + '\n'
+      + 'Connected successfully to ' + config.dbName + ' database'
+    console.log(chalk.green(message))
+    utils.log(message)
+    init(collection, client)
+  }
+})
+
+function init(db, client) {
+  const stamps = {
+    end: timeStamp.toISOString().slice(0,-5)
+  }
+  const msg = 'Getting set ending at ' + stamps.end
+  console.log(msg)
+  utils.log(msg)
+  timeStamp.setHours(timeStamp.getHours() - config.interval_in_hours)
+  stamps.start = timeStamp.toISOString().slice(0,-5)
+  const options = {
+    hostname: config.targetAPIurl,
+    path: utils.getTargetString(config, stamps),
+    method: 'GET',
+    agent: false,
+    headers: {
+      'User-Agent': 'node-client',
+      'Accept' : 'application/json',
+      'Content-Type': 'application/json',
+    }
+  }
+
+  let data = ''
+  const req = https.request(options, res => {
+    if (res.statusCode == 200) console.log('GET call successful')
+    res.on('data', d => {data += d})
+    res.on('end', () => {
+      data = JSON.parse(data)
+      data = utils.normData(data)
+      utils.insertData(client, db, data, recurse)
+    })
+  })
+  req.on('error', err => {
+    console.error(chalk.red(err))
+    utils.log('Request Error: ' + err)
+    utils.endProcess(1, client)
+  })
+  req.end()
+}
+
+function recurse() {
+  setTimeout(() => {
+    init(app.db, app.client)
+  }, 2000)
+}
---
+++ src/utils.js
@@ -0,0 +1,116 @@
+'use strict'
+const chalk = require('chalk')
+const fs = require('fs')
+
+const config = require('../config.json')
+
+module.exports = {
+  getTargetString: getTargetString,
+  insertData: insertData,
+  log: log,
+  normData: normData,
+  endProcess: endProcess
+}
+
+let logger = fs.createWriteStream(config.logDir + '/data-minion.log.txt', {'flags': 'a'})
+
+function getTargetString (config, times) {
+  let res =  config.targetProductString.map(function(x){
+    switch (x) {
+      case 'start': return times.start
+      case 'end': return times.end
+      case 'targetProduct': return config.targetProduct
+      default : return x
+    }
+  })
+  res = res.join('')
+  return res
+}
+
+function insertData (client, db, data, callback) {
+  if (Array.isArray(data)) {
+    db.insertMany(data, function(err, r){
+      if (err) catchInsertManyError(data, err)
+      else {
+        console.log('Data inserted')
+        log('Data inserted')
+      }
+      // else console.log('Insert result: ' + JSON.stringify(r))
+      callback(client)
+    })
+  }
+  else {
+    console.error(chalk.red('API Error: ') + JSON.stringify(data))
+    log('API Error: ' + JSON.stringify(data))
+    endProcess(1, client)
+  }
+}
+
+function catchInsertManyError (data,err) {
+  console.log(chalk.blue(err))
+  log('insertManyError: ' + err)
+}
+
+function log(data) {
+  logger.write(nowstamp() + data + '\n', 'utf8')
+}
+function nowstamp() {return (new Date(Date.now()).toISOString() + ': ')}
+
+function normData (data) {
+  if (data.message) return data
+  else {
+    // map data to our schema
+    data = data.map((x) => {
+      return {
+        _id: x[0],
+        low: x[1],
+        high: x[2],
+        open: x[3],
+        close: x[4],
+        volume:x[5]  
+      }
+    })
+    // insert empty candles
+    const res = []
+    const l = data.length
+    let i = 0
+    while(i < l) {
+      // make sure to enter first candle
+      if (i == 0) {
+        res.push(data[i])
+      }
+      else {
+        // push sequentia candles
+        if (Number(data[i -1]._id) - Number(data[i]._id) === 60) res.push(data[i])
+        else {
+          // or enter empty candles
+          const offset = ((Number(data[i - 1]._id) - Number(data[i]._id)) / 60) - 1
+          let c = 0
+          while (c < offset) {
+            res.push({
+              _id: Number(data[i - 1]._id) - ((c + 1) * 60),
+              volume: 0
+            })
+            c++
+          }
+          // then push the current candle
+          res.push(data[i])
+        }
+      }
+      i++
+    }
+    if (i === l) {
+      return res
+    }
+  }
+}
+
+function endProcess (err, client) {
+  if (err == null) {
+    console.log(chalk.green('Process complete'))
+    log('Process complete')
+  }
+  client.close()
+  logger.end()
+  process.exit()
+}
